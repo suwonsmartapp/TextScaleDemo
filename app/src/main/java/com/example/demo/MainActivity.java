19b3e32729def42fdaf4cf465b714c883dc8caca
Junsuk Oh <a811219@gmail.com>
2017-03-25 11:35:57 +0900

    이미지 확대 적용

--- app/src/main/java/com/example/demo/MainActivity.java
+++ app/src/main/java/com/example/demo/MainActivity.java
@@ -1,21 +1,26 @@
 package com.example.demo;
 
+import android.graphics.Matrix;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final int DEFAULT_TEXT_SIZE = 16;
+    public static final int DEFAULT_IMAGE_WIDTH = 50;
+    public static final int DEFAULT_IMAGE_HIGHT = 50;
 
     private EditText mEditText;
     private TextView mTextView;
     private SeekBar mSeekBar;
     private TextView mScaleTextView;
+    private ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         mTextView = (TextView) findViewById(R.id.text_text);
         mScaleTextView = (TextView) findViewById(R.id.scale_text);
         mSeekBar = (SeekBar) findViewById(R.id.seekBar);
+        mImageView = (ImageView) findViewById(R.id.image_view);
 
         // 문자 변경시 호출 되는 콜백
         mEditText.addTextChangedListener(new TextWatcher() {
@@ -54,6 +60,12 @@ public class MainActivity extends AppCompatActivity {
 
                 // 글자 확대
                 mTextView.setTextSize(DEFAULT_TEXT_SIZE * progress + DEFAULT_TEXT_SIZE);
+
+                // 이미지 배율
+                Matrix matrix = new Matrix();
+                matrix.postScale((progress + 1),
+                        (progress + 1));
+                mImageView.setImageMatrix(matrix);
             }
 
             @Override
